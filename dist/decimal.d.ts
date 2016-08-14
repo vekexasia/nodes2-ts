20a77a0827eeef29ac20abcef335778286337683
Andrea Baccega <vekexasia@gmail.com>
2016-08-14 18:37:44 +0200

    added missing files

---
+++ dist/decimal.d.ts
@@ -0,0 +1,1 @@
+export declare const Decimal: decimal.IDecimalStatic;
---
+++ dist/decimal.js
@@ -0,0 +1,13 @@
+(function (factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    exports.Decimal = require('decimal.js');
+    ;
+});
+//# sourceMappingURL=decimal.js.map
