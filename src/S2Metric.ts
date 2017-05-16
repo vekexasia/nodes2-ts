e2432ad167dbf470a15d9256c69f4263e8a05cc2
Dane Pieri <danepieri@Danes-Powerbook-G4.local>
2017-05-16 15:58:15 -0700

    Implement getValue method on S2Metric

--- src/S2Metric.ts
+++ src/S2Metric.ts
@@ -26,7 +26,8 @@ export class S2Metric {
 
     /** Return the value of a metric for cells at the given level. */
     public getValue(level:number):number {
-        return 0;
+        let scaleFactor = this._dim * (1 - level);
+        return this._deriv.toNumber() * Math.pow(scaleFactor, 2);
         // return StrictMath.scalb(deriv, dim * (1 - level));
     }
 
