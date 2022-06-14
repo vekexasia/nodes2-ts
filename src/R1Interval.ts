ee1b551c6c7cfa62a8da1c208661365e6e8119d2
Andrea Baccega <vekexasia@gmail.com>
2022-06-14 12:22:56 +0200

    init eslint

--- src/R1Interval.ts
+++ src/R1Interval.ts
@@ -142,7 +142,7 @@ export class R1Interval extends Interval {
    * Return true if the length of the symmetric difference between the two
    * intervals is at most the given tolerance.
    */
-  public approxEquals(y:R1Interval, maxError:number=1e-15):boolean {
+  public approxEquals(y:R1Interval, maxError=1e-15):boolean {
     if (this.isEmpty()) {
       return y.getLength() <= (maxError);
     }
